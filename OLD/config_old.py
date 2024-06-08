38df422779ad9ac5b5a4299f92085f92f3f9df0c
dacostapiece <101139745+dacostapiece@users.noreply.github.com>
2024-06-08 02:01:38 -0300

    Delete OLD directory

--- OLD/config_old.py
+++
@@ -1,9 +0,0 @@
-# Mail settings
-mailserver = 'isp07.netstructure.com.br'
-smtpport = 587
-mailusername = 'soc@snsec.com.br'
-mailpassword = 'Alter@2024/*'
-source_mailaddress = 'soc@snsec.com.br'
-dest_mailaddress = 'infraestrutura@netstructure.com.br'
-mailsubject_success = "MY RASP TUN IP SERVICE ADDRESS IS: "
-mailsubject_failed = "VPN Failed"
