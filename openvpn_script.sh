a4e2a12cf93eb1f019bc833e382ac45beba54555
Rafael da Costa <dacostapiece@gmail.com>
2024-10-11 21:10:21 -0300

    update files not sync, testing env with openvpn and updating sync services

--- openvpn_script.sh
+++ openvpn_script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Load environment variables from .env file
+source /home/dacosta/CALLHOME/.env
+
 exec &>>/tmp/openvpn_script.log
 sudo openvpn --config /home/dacosta/CALLHOME/hub.ovpn --auth-user-pass /home/dacosta/CALLHOME/pass.txt
-
--- sync_services_scripts.sh
+++ sync_services_scripts.sh
@@ -4,4 +4,3 @@ cat /etc/systemd/system/ovpnscript.service >/home/dacosta/CALLHOME/SERVICES/ovpn
 cat /etc/systemd/system/updatedns.service >/home/dacosta/CALLHOME/SERVICES/updatedns.service
 cat /etc/systemd/system/vpnstatuspanel.service >/home/dacosta/CALLHOME/SERVICES/vpnstatuspanel.service
 cat /etc/systemd/system/autossh.service >/home/dacosta/CALLHOME/SERVICES/autossh.service
-cat /etc/systemd/system/ssh-agent.service >/home/dacosta/CALLHOME/SERVICES/ssh-agent.service
